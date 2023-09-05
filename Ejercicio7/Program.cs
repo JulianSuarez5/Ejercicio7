e1e78e8425aa276eaea956cf5253e85ea9012902
JulianSuarez5 <Juacosuarez12@gmail.com>
2023-09-04 22:33:47 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Factorial
+{
+    static void Main()
+    {
+        double n, f;
+
+        Console.Write("Ingrese un número: ");
+        n = double.Parse(Console.ReadLine());
+
+        f = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            f = f * i;
+        }
+
+        Console.WriteLine("El factorial de {0} es {1}", n, f);
+
+        Console.ReadKey();
+    }
+}
